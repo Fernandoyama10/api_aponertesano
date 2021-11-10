2e6a2be9397ac8b4d7c0bbfbd2f2bfd22c67b05e
Fernandoyama10 <74337206+Fernandoyama10@users.noreply.github.com>
2021-11-10 12:25:44 -0600

    cambios de get a post api caloriees
    cambios de get a post api para recibir body

--- src/routes/calories.js
+++ src/routes/calories.js
@@ -2,7 +2,7 @@ const caloriesController = require('../controllers/calories');
 const express = require('express');
 const router = express.Router();
 
-router.get('/calories', caloriesController.caloriesdata);
+router.post('/calories', caloriesController.caloriesdata);
 router.get('/caloriesget/:id_user', caloriesController.caloriesget);
 
 
