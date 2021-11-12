193fa2e0fbe4d7653b52b02c25cdd612647628cb
beds10 <brandonsjn1@gmail.com>
2021-11-12 14:45:59 -0600

    Act Api Food

--- src/controllers/interfood.js
+++ src/controllers/interfood.js
@@ -3,8 +3,8 @@ const mysql = require("mysql");
 
 exports.registerfood = async (req, res) => {
     try {
-        const { date_r, time, label, image, quantity, calories, protein, fat, carbs, sugar, sodium, id_user, id_status } = req.body;
-        mysqlConnection.query('INSERT INTO meal_record set ?', { date_r: date_r, time: time, label: label, image: image, quantity: quantity, calories: calories, protein: protein, fat: fat, carbs: carbs, sugar: sugar, sodium: sodium, id_user: id_user }, (error, resultado) => {
+        const { date_r, time, label, image, type, quantity, calories, protein, fat, carbs, sugar, sodium, id_user, id_status } = req.body;
+        mysqlConnection.query('INSERT INTO meal_record set ?', { date_r: date_r, time: time, label: label, image: image, type: type, quantity: quantity, calories: calories, protein: protein, fat: fat, carbs: carbs, sugar: sugar, sodium: sodium, id_user: id_user }, (error, resultado) => {
             if (error) {
                 res.send({ message: "Error en el registro", statusCode: 400 });
             } else {
