c9582ee113250e7adaa82d3d47b79add9fdb537f
beds10 <brandonsjn1@gmail.com>
2021-11-13 10:46:34 -0600

    Cambios Api

--- src/controllers/yucfood.js
+++ src/controllers/yucfood.js
@@ -21,3 +21,37 @@ exports.getyucfood = async (req, res) => {
       }
     });
   }
+
+  exports.getFoodRecord = async (req, res) => {
+    try{ 
+      const { date_r, id_user } = req.body;
+      mysqlConnection.query("SELECT * from meal_record WHERE date_r = ? and  id_user = ? ", [date_r, id_user], (err, rows, fields) => {
+        if (!err) {
+          res.send(rows);
+        } else {
+          console.log(err);
+        }
+      });
+
+  }catch(e){
+      throw e;
+  }
+  }
+
+
+  
+  exports.getCaloriesRecord = async (req, res) => {
+    try{ 
+      const { date_r, id_user } = req.body;
+      mysqlConnection.query("SELECT SUM(calories) as calories, SUM(protein) as protein, SUM(fat) as fat, SUM(carbs) as carbs, SUM(sugar) as sugar from meal_record WHERE date_r = ? and  id_user = ? ", [date_r, id_user], (err, rows, fields) => {
+        if (!err) {
+          res.send(rows);
+        } else {
+          console.log(err);
+        }
+      });
+
+  }catch(e){
+      throw e;
+  }
+  }
--- src/routes/yucfood.js
+++ src/routes/yucfood.js
@@ -4,4 +4,6 @@ const router = express.Router();
 
 router.get('/get-yucfood', yucfoodController.getyucfood);
 router.get('/get-yucfood/:name', yucfoodController.getyucfoodName);
+router.post('/get-records', yucfoodController.getFoodRecord);
+router.post('/get-recordcalories', yucfoodController.getCaloriesRecord);
 module.exports = router;
