eb246535de70cffd573a9bb9491488a64f3d11cb
beds10 <brandonsjn1@gmail.com>
2021-11-13 11:06:04 -0600

    Cambios api

--- src/controllers/calories.js
+++ src/controllers/calories.js
@@ -20,3 +20,20 @@ exports.caloriesdata = async (req, res) => {
     
   }
 
+
+  exports.caloriesget = async (req, res) => {
+    try {
+      const params = req.params;
+
+      mysqlConnection.query('SELECT SUM(calories) as calories, SUM(protein) as protein, SUM(fat) as fat, SUM(carbs) as carbs, SUM(sugar) as sugar FROM meal_record WHERE date_r = "2021-11-09" and ?', [params], (err, rows, fields) => {
+        if (!err) {
+  
+          res.send(rows);
+        } else {
+          console.log(err);
+        }
+      });
+    } catch (e) {
+      throw e;
+    }
+  }
