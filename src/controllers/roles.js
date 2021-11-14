61957dc00f6b86c6e60997952ae33da4940630a6
PedroUrtecho90 <75145945+PedroUrtecho90@users.noreply.github.com>
2021-11-14 16:33:39 -0600

    API_Roles
    Creacion de  CRUD de Roles

--- src/controllers/roles.js
+++ src/controllers/roles.js
@@ -1,6 +1,8 @@
 const mysqlConnection = require('../database');
 const mysql = require("mysql");
 const { request } = require('../../app');
+const app = require('../../app');
+const e = require('express');
 
 exports.getroles = async (req, res) => {
   mysqlConnection.query('SELECT * FROM roles', (err, rows, fields) => {
@@ -13,22 +15,41 @@ exports.getroles = async (req, res) => {
 }
 
 exports.addrole = async (req, res) => {
-  try {
-    const {role_name} = req.body;
-    mysqlConnection.query('INSERT INTO roles set ?', { role_name: role_name}, (error, resultado) => {
-      
-      if (error) {
-        console.log(res);
-        res.send({ message: "Error", statusCode: 400 });
+  const { role_name } = req.body;
+  mysqlConnection.query('INSERT INTO roles set ?', { role_name: role_name }, (error, resultado) => {
+    if (error) {
+      res.send({ message: "Error en el registro", statusCode: 400 });
+      res.json(error);
+    } else {
+      res.json(resultado);
+      console.log('Se agrego el registro');
+    }
+  })
+}
 
-      } else 
-      {
-            console.log('Se registro totalmente');
-            res.send(resultado);
-      }
-    });
-  } catch (e) 
-  {
-    throw e;
-  }
+exports.deleterol = async (req, res) => {
+  const params = req.params;
+  mysqlConnection.query('DELETE from roles WHERE  ?', [params], (error, resultado) => {
+    if (error) {
+      res.send({ message: "Error en el registro", statusCode: 400 });
+      res.json(error);
+    }
+    else {
+      res.json(resultado);
+      console.log('Se elimino el registro');
+    }
+  })
+}
+
+exports.updateroles = async (req, res) => {
+  const { role_name, id_role } = req.body;
+  mysqlConnection.query('UPDATE roles set role_name = ? WHERE id_role = ?', [role_name, id_role], (error, resultado) => {
+    if (error) {
+      res.send({ message: "Error en el registro", statusCode: 400 });
+      res.json(error);
+    } else {
+      res.json(resultado);
+      console.log('Se actualizo el registro');
+    }
+  })
 }
--- src/routes/roles.js
+++ src/routes/roles.js
@@ -4,5 +4,7 @@ const router = express.Router();
 
 router.get('/get-roles', rolesController.getroles );
 router.post('/add-roles', rolesController.addrole );
+router.delete('/delete-roles/:id_role', rolesController.deleterol );
+router.post('/update-roles/:id_role', rolesController.updateroles );
 
 module.exports = router;
