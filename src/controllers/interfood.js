f02fb2666ce6a95cfbb652b9ae2f6c516f04d0b5
beds10 <brandonsjn1@gmail.com>
2021-11-15 13:26:00 -0600

    Cambio Initial Calories Registro

--- src/controllers/interfood.js
+++ src/controllers/interfood.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 
 exports.registerfood = async (req, res) => {
     try {
-        const { date_r, time, label, image, type, quantity, calories, protein, fat, carbs, sugar, sodium, id_user, id_status } = req.body;
+        const { date_r, time, label, image, type, quantity, calories, protein, fat, carbs, sugar, sodium, id_user, id_status, initial_calories } = req.body;
         mysqlConnection.query('INSERT INTO meal_record set ?', { date_r: date_r, time: time, label: label, image: image, type: type, quantity: quantity, calories: calories, protein: protein, fat: fat, carbs: carbs, sugar: sugar, sodium: sodium, id_user: id_user }, (error, resultado) => {
             if (error) {
                 res.send({ message: "Error en el registro", statusCode: 400 });
@@ -18,7 +18,7 @@ exports.registerfood = async (req, res) => {
                             console.log('Se creo el registro el platillo y se sumaron las calorias porque ya existian en la tabla');
                         } else {
 
-                            mysqlConnection.query('INSERT INTO calories_result SET ?', { date: date_r, final_calories: calories, id_status: id_status, id_user: id_user }, (error, resultss) => {
+                            mysqlConnection.query('INSERT INTO calories_result SET ?', { date: date_r, initial_calories: initial_calories, final_calories: calories, id_status: id_status, id_user: id_user }, (error, resultss) => {
                                 if (error) {
                                     res.send({ message: "Error en el registro", statusCode: 400 });
                                 } else {
