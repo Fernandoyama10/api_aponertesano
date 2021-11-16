e6bcc75bd899f65779532454b90c94a72d3e98a9
beds10 <brandonsjn1@gmail.com>
2021-11-16 13:41:18 -0600

    xd

--- src/controllers/yucfood.js
+++ src/controllers/yucfood.js
@@ -2,74 +2,6 @@ const mysqlConnection = require('../database');
 const mysql = require("mysql");
 
 exports.getyucfood = async (req, res) => {
-<<<<<<< HEAD
-
-  mysqlConnection.query('SELECT * FROM yucatan_food', (err, rows, fields) => {
-    if (!err) {
-      res.json(rows);
-    } else {
-      console.log(err);
-    }
-  });
-}
-
-exports.getyucfoodID = async (req, res) => {
-  const params = req.params;
-  mysqlConnection.query('SELECT * FROM yucatan_food WHERE ?', [params], (err, rows, fields) => {
-    if (!err) {
-      res.json(rows[0]);
-    } else {
-      console.log(err);
-    }
-  });
-}
-
-
-exports.addyucafood = async (req, res) => {
-  const { food_name, image, tipo, quantity, calories, protein, fat, carbs, sugar, sodium } = req.body;
-  mysqlConnection.query('INSERT INTO yucatan_food set ?', {food_name:food_name, image:image, tipo: tipo, quantity:quantity, calories:calories, protein:protein, fat:fat, carbs:carbs, sugar:sugar, sodium:sodium}, (error, resultado) => {
-    if (error) {
-      res.send({ message: "Error al REGISTRAR", statusCode: 400 });
-      res.json(error);
-    } else {
-      res.json(resultado);
-      console.log('Se agrego el registro');
-    }
-  })
-}
-
-
-exports.deleteyucafood = async (req, res) => {
-  const params = req.params;
-  mysqlConnection.query('DELETE from yucatan_food WHERE  ?', [params], (error, resultado) => {
-    if (error) {
-      res.send({ message: "Error al ELIMINAR", statusCode: 400 });
-      res.json(error);
-    }
-    else {
-      res.json(resultado);
-      console.log('Se elimino el registro');
-    }
-  })
-}
-
-
-exports.updateyucafood = async (req, res) => {
-  const {food_name, image, tipo, quantity, calories, protein, fat, carbs, sugar, sodium } = req.body;
-  const {id_yucatan_food} = req.params;
-  mysqlConnection.query('UPDATE yucatan_food SET ? WHERE id_yucatan_food = ?', [{food_name, image, tipo, quantity, calories, protein, fat, carbs, sugar, sodium }, id_yucatan_food] , (error, resultado) => {
-    if (error) {
-      res.send({ message: "Error al ACTUALIZAR", statusCode: 400 });
-      res.json(error);
-    } else {
-      res.json(resultado);
-      console.log('Se actualizo el registro');
-    }
-  })
-}
-
-   
-=======
     mysqlConnection.query('SELECT * FROM yucatan_food', (err, rows, fields) => {
       if (!err) {
 
@@ -79,7 +11,6 @@ exports.updateyucafood = async (req, res) => {
       }
     });
   }
->>>>>>> parent of a871207 (Merge remote-tracking branch 'origin/Develop_D' into Develop_B)
 
   exports.getyucfoodName = async (req, res) => {
     const params = req.params;
@@ -124,10 +55,4 @@ exports.updateyucafood = async (req, res) => {
   }catch(e){
       throw e;
   }
-<<<<<<< HEAD
   }
-
-    
-=======
-  }
->>>>>>> parent of a871207 (Merge remote-tracking branch 'origin/Develop_D' into Develop_B)
--- src/routes/users.js
+++ src/routes/users.js
@@ -6,16 +6,9 @@ router.get('/get-users', usersController.getusers);
 router.get('/get-users/:id_user', usersController.getByID);
 router.get('/get-infouser/:id_user', usersController.getByInfoUserID);
 router.get('/login/:email', usersController.login);
-<<<<<<< HEAD
-router.post('/register', usersController.register);
-router.put('/update', usersController.update);
-
-
-=======
 
 router.post('/register', usersController.register);
 router.put('/update', usersController.update);
->>>>>>> parent of a871207 (Merge remote-tracking branch 'origin/Develop_D' into Develop_B)
 
 router.get('/logindata/:email', usersController.logindata);
 
