13dc99208c78364b5e7133d0d543786556007c31
beds10 <brandonsjn1@gmail.com>
2021-11-16 12:29:33 -0600

    xd

--- src/routes/yucfood.js
+++ src/routes/yucfood.js
@@ -3,10 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/get-yucfood', yucfoodController.getyucfood);
+router.get('/get-yucfood/:name', yucfoodController.getyucfoodName);
+router.get('/get-records/:date_r/:id_user', yucfoodController.getFoodRecord);
+router.get('/get-recordcalories/:date_r/:id_user', yucfoodController.getCaloriesRecord);
+
 router.get('/get-yucfood/:id_yucatan_food', yucfoodController.getyucfoodID);
 router.post('/add-yucfood/', yucfoodController.addyucafood);
 router.delete('/delete-yucfood/:id_yucatan_food', yucfoodController.deleteyucafood);
 router.put('/update-yucfood/:id_yucatan_food', yucfoodController.updateyucafood);
-
-
 module.exports = router;
