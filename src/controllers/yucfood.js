c05e84926a6a5a9b08db002c2e586c0d698e5eb3
beds10 <brandonsjn1@gmail.com>
2021-11-18 18:18:54 -0600

    Status API

--- src/controllers/yucfood.js
+++ src/controllers/yucfood.js
@@ -44,7 +44,7 @@ exports.getyucfood = async (req, res) => {
   
   exports.getCaloriesRecord = async (req, res) => {
     try{ 
-      mysqlConnection.query("SELECT SUM(calories) as calories, SUM(protein) as protein, SUM(fat) as fat, SUM(carbs) as carbs, SUM(sugar) as sugar, SUM(sodium) as sodium, initial_calories from meal_record M join calories_result C ON M.id_user = C.id_user AND M.date_r = C.date WHERE C.date = ? and M.id_user = ? ", [req.params.date_r, req.params.id_user], (err, rows, fields) => {
+      mysqlConnection.query("SELECT SUM(calories) as calories, SUM(protein) as protein, SUM(fat) as fat, SUM(carbs) as carbs, SUM(sugar) as sugar, SUM(sodium) as sodium, initial_calories, C.id_status, name_status FROM meal_record as M join calories_result as C ON M.id_user = C.id_user and M.date_r = C.date join status as S ON S.id_status = C.id_status WHERE C.date = ? and M.id_user = ? ", [req.params.date_r, req.params.id_user], (err, rows, fields) => {
         if (!err) {
           res.send(rows);
         } else {
